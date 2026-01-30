e56d36d4044bcc2a35d06bd5dc6d69f018a2afd3
romaz <35759448+romaz21@users.noreply.github.com>
2026-01-30 07:58:51 +0300

    v

--- crates/sui-protocol-config/src/lib.rs
+++ crates/sui-protocol-config/src/lib.rs
@@ -2640,18 +2640,18 @@ impl ProtocolConfig {
     /// Get the value ProtocolConfig that are in effect during the given protocol version.
     pub fn get_for_version(version: ProtocolVersion, chain: Chain) -> Self {
         // ProtocolVersion can be deserialized so we need to check it here as well.
-        assert!(
-            version >= ProtocolVersion::MIN,
-            "Network protocol version is {:?}, but the minimum supported version by the binary is {:?}. Please upgrade the binary.",
-            version,
-            ProtocolVersion::MIN.0,
-        );
-        assert!(
-            version <= ProtocolVersion::MAX_ALLOWED,
-            "Network protocol version is {:?}, but the maximum supported version by the binary is {:?}. Please upgrade the binary.",
-            version,
-            ProtocolVersion::MAX_ALLOWED.0,
-        );
+        // assert!(
+        //     version >= ProtocolVersion::MIN,
+        //     "Network protocol version is {:?}, but the minimum supported version by the binary is {:?}. Please upgrade the binary.",
+        //     version,
+        //     ProtocolVersion::MIN.0,
+        // );
+        // assert!(
+        //     version <= ProtocolVersion::MAX_ALLOWED,
+        //     "Network protocol version is {:?}, but the maximum supported version by the binary is {:?}. Please upgrade the binary.",
+        //     version,
+        //     ProtocolVersion::MAX_ALLOWED.0,
+        // );
 
         let mut ret = Self::get_for_version_impl(version, chain);
         ret.version = version;
@@ -4616,7 +4616,7 @@ impl ProtocolConfig {
                         cfg.feature_flags.defer_unpaid_amplification = true;
                     }
                 }
-                114 => {
+                114 | _ => {
                     cfg.feature_flags.randomize_checkpoint_tx_limit_in_tests = true;
                 }
                 // Use this template when making changes:
